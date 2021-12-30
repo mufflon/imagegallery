52eae96e3ce961b4cc83de6d28a70d65915e7127
Alexander Wallin <alexander@wallindevelopment.se>
2021-12-30 20:29:53 +0100

    correct tag

--- localcode_mac.sh
+++ localcode_mac.sh
@@ -16,4 +16,4 @@ mkdir "$NEWOUTPUT"
 
 echo "output is $NEWOUTPUT"
 echo "will now run docker"
-docker run --mount type=bind,source="$1",target=/usr/src/app/source --mount type=bind,source="$NEWOUTPUT",target=/usr/src/app/dest mufflonicus/imagelibrary
+docker run --mount type=bind,source="$1",target=/usr/src/app/source --mount type=bind,source="$NEWOUTPUT",target=/usr/src/app/dest mufflonicus/imagelibrary:1.0.0
